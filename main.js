7da0a13cc658d1ae51bcb621fb93de5b1f736d86
Paweł Kuniec <pawllo1992@vp.pl>
2022-03-04 11:48:24 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert('Co taaaaaaaaam? hakujemy Kasi fejsa?')
+console.log ('siema');
+const name= 'Paweł';
+const age= 29;
+console.log(name);
+console.log(age);
+
+console.log(`nazywam się ${name} i mam ${age} lat`);
